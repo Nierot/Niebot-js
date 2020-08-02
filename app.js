e1cd304095ddef382f7374e810feef218591b53b
Niels <git@nierot.com>
2020-08-02 20:02:43 +0200

    Added command system

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const client = require('discord.js').Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const settings = require('./settings.json');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+client.on('message', msg => {
+    if (!msg.content.startsWith(settings.prefix) || msg.author.bot) return; // If not a command and not send by the bot
+    
+    const args = msg.content.slice(settings.prefix.length).trim().split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === 'ping') {
+        msg.channel.send('pong');
+    }
 });
 
-client.on('message', msg => {
-    if (msg.content = 'ping') msg.reply('pong');
+
+// Login and initialize
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.login(settings.token);
