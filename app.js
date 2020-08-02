14cdb6d5c925de27a2c67e4595659f3d431e4c24
Niels <git@nierot.com>
2020-08-02 20:10:05 +0200

    Command system with modules

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const settings = require('./settings.json');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./settings.json');
+client.commands = new Discord.Collection();
+
 
 client.on('message', msg => {
     if (!msg.content.startsWith(settings.prefix) || msg.author.bot) return; // If not a command and not send by the bot
@@ -8,14 +12,23 @@ client.on('message', msg => {
     const args = msg.content.slice(settings.prefix.length).trim().split(/ +/);
     const cmd = args.shift().toLowerCase();
 
-    if (cmd === 'ping') {
-        msg.channel.send('pong');
+    if (!client.commands.has(cmd)) return; // Command does not exist
+
+    try {
+        client.commands.get(cmd).execute(msg, args);
+    } catch (error) {
+        console.error(error);
+        msg.reply('Lekker bezig, bot is stuk');
     }
 });
 
 
 // Login and initialize
 client.on('ready', () => {
+    fs.readdirSync('./commands').filter(file => file.endsWith('.js')).forEach(file => {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
+    });
     console.log(`Logged in as ${client.user.tag}`);
 });
 
--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,7 @@
 module.exports = {
-    
+    name: 'avatar',
+    description: 'Laat de pf zien van de persoon die je tagt',
+    execute(msg, args) {
+        msg.channel.send('pong');
+    }
 }
